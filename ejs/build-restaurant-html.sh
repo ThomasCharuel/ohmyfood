4b94a6f8954615ed0cae24bf58365432afc4191a
ThomasCharuel <ThomasCharuel@users.noreply.github.com>
2023-03-20 14:48:45 +0100

    Improve build restaurant html script

--- ejs/build-restaurant-html.sh
+++ ejs/build-restaurant-html.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ejs ./ejs/restaurant.ejs -f ./ejs/a_la_francaise.json -o ./restaurant__a_la_francaise.html
-ejs ./ejs/restaurant.ejs -f ./ejs/la_note_enchantee.json -o ./restaurant__la_note_enchantee.html
-ejs ./ejs/restaurant.ejs -f ./ejs/la_palette_du_gout.json -o ./restaurant__la_palette_du_gout.html
-ejs ./ejs/restaurant.ejs -f ./ejs/le_delice_des_sens.json -o ./restaurant__le_delice_des_sens.html
+for restaurant_name in a_la_francaise la_note_enchantee la_palette_du_gout le_delice_des_sens
+do
+    cat ./ejs/$restaurant_name.json | ejs ./ejs/restaurant.ejs -o ./restaurant__$restaurant_name.html
+done
